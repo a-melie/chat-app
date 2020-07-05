5f261f5d25c0900c370365fef359856b537464f8
Amelie <a.grn33@gmail.com>
2020-07-05 17:04:10 +0200

    contactlist done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Teresa Silva"
-        avatar= "https://randomuser.me/api/portraits/women/81.jpg"
-              online />
-      <Contact name="Camila Daniels"
-        avatar="https://randomuser.me/api/portraits/women/60.jpg"
-       />
-      <Contact name="Ken Matthews"
-        avatar="https://randomuser.me/api/portraits/men/95.jpg"
-        online />
+     <ContactList />
     </div>
   );
 }
